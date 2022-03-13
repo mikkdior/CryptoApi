6d8fcbe093d063e13cbc53a5add7078368b4a130
DobriyCoder <DobriyCoder@DESKTOP-9RNCKCN>
2022-03-13 11:15:16 +0300

    Добавьте файлы проекта.

---
+++ CryptoApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptoApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
