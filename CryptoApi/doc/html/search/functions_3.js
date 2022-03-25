ef876468231543d3d6d3e33e8357b32728fa7515
Michael <mikkdior@gmail.com>
2022-03-25 13:21:10 +0200

    сделан таймер

---
+++ CryptoApi/doc/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['error_0',['Error',['../class_coin_pairs_controller.html#aacab99cb6f38a56334b3940f57dd462d',1,'CoinPairsController.Error()'],['../class_coins_controller.html#a9c8a2c1c7f5bb2960ece2e448773fd20',1,'CoinsController.Error()'],['../class_home_controller.html#adc49f0c0411d8867649e540a082a18c5',1,'HomeController.Error()']]]
+];
---
+++ CryptoApi/doc/html/search/functions_9.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['settext_0',['SetText',['../class_c_text_block_builder.html#a8d4d91b0ca026fa44015d86295263541',1,'CTextBlockBuilder']]],
+  ['settextdata_1',['SetTextData',['../class_c_text_block_builder.html#a1342b28183690d52b5ec3cab5adccd4a',1,'CTextBlockBuilder']]],
+  ['settextvalues_2',['SetTextValues',['../class_c_text_block_builder.html#a2119a63ea29f9cac551e5df109bf1448',1,'CTextBlockBuilder']]],
+  ['settitle_3',['SetTitle',['../class_c_text_block_builder.html#a1b8fc248ab64f60fedbe4f350e012266',1,'CTextBlockBuilder']]],
+  ['settitledata_4',['SetTitleData',['../class_c_text_block_builder.html#af77c59e43a509cf4077ec5a71126eb5c',1,'CTextBlockBuilder']]],
+  ['settitlevalues_5',['SetTitleValues',['../class_c_text_block_builder.html#a924e689c1f251c151b4c5514b69ada7b',1,'CTextBlockBuilder']]],
+  ['stop_6',['Stop',['../class_actualizer_controller.html#a668e3876a1d5848dee0bc729f5977f8b',1,'ActualizerController']]]
+];
