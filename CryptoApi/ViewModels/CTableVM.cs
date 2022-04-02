260777039c2c9bc35aa8a70ee522b5b7bb2d6203
DobriyCoder <DobriyCoder@DESKTOP-9RNCKCN>
2022-04-02 14:50:15 +0300

    стратегия для таблиц

---
+++ CryptoApi/ViewModels/CTableVM.cs
@@ -0,0 +1,46 @@
+﻿namespace CryptoApi.ViewModels
+{
+    public class CTableVM<TRow> : ITableVM
+    {
+        public delegate string DGetItem(TRow data);
+        private Dictionary<string, DGetItem> items = new();
+        public IEnumerable<string> keys => items.Keys;
+        public IEnumerable<IEnumerable<string>> rows => GetRows();
+
+        IEnumerable<TRow> data { get; set; }
+
+        public CTableVM (IEnumerable<TRow> data)
+        {
+            this.data = data;
+        }
+        public IEnumerable<DGetItem> GetEnumerable()
+        {
+            foreach (var item in items)
+            {
+                yield return item.Value;
+            }
+        }
+
+        public CTableVM<TRow> Add (string title, DGetItem callback)
+        {
+            items.Add (title, callback);
+            return this;
+        }
+
+        private IEnumerable<IEnumerable<string>> GetRows ()
+        {
+            IEnumerable<string> GetItems (TRow row)
+            {
+                foreach(var item in items)
+                {
+                    yield return item.Value(row);
+                }
+            }
+
+            foreach(var row in data)
+            {
+                yield return GetItems(row);
+            }
+        }
+    }
+}
