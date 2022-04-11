1b109d9451a0f689260c3870cf5e7e7981c14642
Michael <mikkdior@gmail.com>
2022-04-11 19:40:02 +0300

    фикс

--- CryptoApi/wwwroot/js/main.js
+++ CryptoApi/wwwroot/js/main.js
@@ -240,13 +240,13 @@ jQuery('.dc-faq').dcTpl(function ($, Export) {
 
 //faq hide/open answers
 document.querySelectorAll('.dc-faq').forEach((faq) => {
-    document.querySelectorAll('.dc-section-text-block').forEach((main) => {
+    document.querySelectorAll('.dc-faq-item').forEach((main) => {
         
         function isAnswerOpened(answer) {
             return answer.style.display != 'none';
         }
         function toggle(main) {
-            let answer = main.parentNode.querySelector('.dc-section-text-block__content');
+            let answer = main.parentNode.querySelector('.dc-faq-item__content');
             let toggle_view = main.querySelector('.dcg-btn .dcg-toggle_view');
             let toggle_hide = main.querySelector('.dcg-btn .dcg-toggle_hide');
 
