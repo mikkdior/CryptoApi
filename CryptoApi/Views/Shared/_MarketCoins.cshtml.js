c125e26c3ae0f3d7869bd5ea6a45b8c35400c117
Michael <mikkdior@gmail.com>
2022-04-12 16:45:42 +0300

    ы

--- CryptoApi/Views/Shared/_MarketCoins.cshtml.js
+++ CryptoApi/Views/Shared/_MarketCoins.cshtml.js
@@ -1,6 +1,12 @@
 // market-coins
 jQuery('.dc-market-coins').dcTpl(function ($, Export) {
-   var $self = $(this);
+    var $self = $(this);
+
+    /*$self.on('click', '.dc-faq-item__wrapper', function (e) {
+        var $item = $(this);
+        $item.closest('.dc-faq-item').find('.dc-faq-item__content').stop().slideToggle(); // parent
+        $item.toggleClass('dc-faq-item__wrapper_opened');
+    });*/
 });
 // /market-coins
 //--------------------------------------------
