b2bcd43eb3f67b5072884fc210644c1eba2b751e
Michael <mikkdior@gmail.com>
2022-04-12 12:26:37 +0300

    faq доработан

--- CryptoApi/Views/Shared/_Faq.cshtml.js
+++ CryptoApi/Views/Shared/_Faq.cshtml.js
@@ -2,17 +2,15 @@
 jQuery('.dc-faq').dcTpl(function ($, Export) {
     var $self = $(this);
 
-    var $coll = $self.find('.dc-faq-item__wrapper');
-
+    /*var $coll = $self.find('.dc-faq-item__wrapper');*/
     /* $coll.on('click', function (e) {
         console.log('cllick!!');
     });*/
-
+     
     $self.on('click', '.dc-faq-item__wrapper', function (e) {
-        console.log('cllick!!');
         var $item = $(this);
         $item.closest('.dc-faq-item').find('.dc-faq-item__content').stop().slideToggle(); // parent
-        $item.toggleClass('.dc-faq-item__wrapper_opened');
+        $item.toggleClass('dc-faq-item__wrapper_opened');
     });
 
 });
